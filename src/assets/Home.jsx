c5bbf7f75b15ca712f73ec1962fcf643293c9f50
apilbasnet <117279433+apilbasnet@users.noreply.github.com>
2023-04-28 10:17:24 +0545

    a

--- src/assets/Home.jsx
+++ src/assets/Home.jsx
@@ -7,13 +7,18 @@ export default function home() {
   return (
     <div className='h-screen snap-center flex items-center mx-24'>
       <h1 className='text-8xl  font-semibold' >
-        Apil <br /> Basnet <br />
+        Apil 
+        <h1>
+        Basnet 
+        </h1>
+
         <p className='text-5xl'>
           Frontend Developer
-          <br />
+         
+          <div>
           <button className='bg-gray-900 rounded-none border-none box-border text-white cursor-pointer inline-block font-bold text-base  m-3 min-h-11 min-w-10 outline-none overflow-hidden px-5 py-2 relative text-center select-none touch-manipulation hover:opacity-75'>  About me</button>
           <button className='bg-gray-900 rounded-none border-none box-border text-white cursor-pointer inline-block font-bold text-base  m-0 min-h-11 min-w-10 outline-none overflow-hidden px-5 py-2 relative text-center select-none touch-manipulation hover:opacity-75'>Hire me </button>
-        455165165
+          </div>  
         </p>
 
       </h1>
