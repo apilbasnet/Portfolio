7ad1203a6c0d0a093099bfbe10f4921b0f2a4cf3
apilbasnet <117279433+apilbasnet@users.noreply.github.com>
2023-05-02 19:32:47 +0545

    gradient

--- src/assets/About.jsx
+++ src/assets/About.jsx
@@ -10,25 +10,25 @@ export default function About() {
 
   return (
     <div className='   flex flex-col mt-20 mb-20'>
-      <div className='   min-w-screen   bg-black text-white    w-full    '>
-        <div className='flex justify-evenly text-center  py-10  mx-2 lg:max-w-[1140px] lg:mx-auto  font-medium text-xl  '>
-          <h2 className='  text-2xl font-semibold'>
+      <div className='    bg-gradient-to-r  from-blue-500 to-cyan-500  text-white    min-w-screen    '>
+        <div className=' justify-evenly text-center  lg:py-20 py-10  mx-2 lg:max-w-[1140px] lg:mx-auto  font-medium text-xl flex-row md:flex lg:flex '>
+          <h2 className='  text-3xl font-semibold pb-5'>
             SKILLS
-            <p className='text-sm text-left font-medium'>
+            <p className='text-base lg:text-left font-medium'>
               I am an enthusiastic learner.
               Here are couple of languages and tools
-              <div> I've acquired the skills for.</div>
+              <div> I've acquired the skills for:</div>
             </p>
           </h2>
 
-          <div className='flex flex-col ' >
+          <div className='flex flex-col font-bold' >
             <a href="https://www.gnu.org/software/gnu-c-manual/gnu-c-manual.html" target='_blank' rel='noreferrer'>C</a>
             <a href="https://react.dev/" target='_blank' rel='noreferrer'>ReactJS</a>
             <a href="https://tailwindcss.com/" target='_blank' rel='noreferrer'>Tailwind</a>
 
           </div>
 
-          <div className='flex flex-col'>
+          <div className='flex flex-col font-bold'>
             <a href="https://developer.mozilla.org/en-US/docs/Web/HTML" target='_blank' rel='noreferrer'>HTML/Css</a>
             <a href="https://code.visualstudio.com/" target='_blank' rel='noreferrer'>VS Code</a>
             <a href="https://www.figma.com/" target='_blank' rel='noreferrer'>Figma</a>
@@ -45,13 +45,13 @@ export default function About() {
           ABOUT ME
         </h2>
         <p >
-          Hi, My name is <b>Apil Basnet</b> and I am a  <b>student</b>  and  self-taught <b>Front-end developer from Nepal.</b> I love experimenting on UI/UX and transforming in to such creative websites.
+          Hi, My name is <b>Apil Basnet</b> and I am a  <b>student</b>  and  self-taught <b>Front-end developer from Nepal.</b> I love experimenting on UI/UX and transforming it in to such creative websites.
         </p>
         <p>
           <br /> Currently studying <b>Bachelors in Computer Application</b>  through <b> Tribhuwan University. </b> Expecting to graduate in <b>2025.</b>
         </p>
         <p>
-          <br />  Aside from coding, I enjoy <b> playing guitars </b> and <b> grinding in competitive FPS shooters.</b>
+          <br />  Aside from coding, I enjoy <b> playing guitar </b> and <b> grinding in competitive FPS shooters.</b>
         </p>
 
 
--- src/assets/Home.jsx
+++ src/assets/Home.jsx
@@ -5,30 +5,47 @@ export default function home() {
 
 
   return (
-    <div className='      h-[calc(100vh-50px)] snap-center flex items-center mx-2 lg:max-w-[1140px] lg:mx-auto'>
-      <h1 className='text-8xl  font-semibold' >
-        Apil 
-        <h1>
-        Basnet 
+    <>
+      <div className='      h-[calc(100vh-50px)] snap-center flex items-center mx-2 lg:max-w-[1140px] lg:mx-auto'>
+        <h1 className='text-8xl  font-semibold' >
+          Apil
+          <h1>
+            Basnet
+          </h1>
+
+          <p className='text-5xl'>
+            Frontend Developer
+
+            <div>
+              <button className='bg-gray-900 rounded-none border-none box-border text-white cursor-pointer inline-block font-bold text-base  m-3 min-h-11 min-w-10 outline-none overflow-hidden px-5 py-2 relative text-center select-none touch-manipulation hover:opacity-75'>  About me</button>
+              <button className='bg-gray-900 rounded-none border-none box-border text-white cursor-pointer inline-block font-bold text-base  m-0 min-h-11 min-w-10 outline-none overflow-hidden px-5 py-2 relative text-center select-none touch-manipulation hover:opacity-75'>Hire me </button>
+            </div>
+          </p>
+
         </h1>
+      </div>
 
-        <p className='text-5xl'>
-          Frontend Developer
-         
-          <div>
-          <button className='bg-gray-900 rounded-none border-none box-border text-white cursor-pointer inline-block font-bold text-base  m-3 min-h-11 min-w-10 outline-none overflow-hidden px-5 py-2 relative text-center select-none touch-manipulation hover:opacity-75'>  About me</button>
-          <button className='bg-gray-900 rounded-none border-none box-border text-white cursor-pointer inline-block font-bold text-base  m-0 min-h-11 min-w-10 outline-none overflow-hidden px-5 py-2 relative text-center select-none touch-manipulation hover:opacity-75'>Hire me </button>
-          </div>  
-        </p>
+      <div className='bg-gradient-to-r  from-blue-500 to-cyan-500  text-white    min-w-screen  flex flex-row  mb-14  '>
+        <div className='  flex-row lg:flex md:flex justify-between  items-center  py-10  mx-2 lg:max-w-[1140px] lg:mx-auto  w-[1140px] font-medium text-lg  '>
 
-      </h1>
+        <div className='text-left'>
+          I like to think creatively and make simple, clear<br />and usable UI/UX for easier navigation.  
+        </div>
 
-
-       
+        <div>
+        <button className=' mx-6 bg-slate-50 rounded-none border-none box-border text-gray-900 cursor-pointer inline-block font-bold text-base  m-3 min-h-11 min-w-10 outline-none overflow-hidden px-5 py-2 relative text-center select-none touch-manipulation hover:opacity-75'>Resume</button>
 
 
+        </div>
 
-    </div>
+        </div>
+      </div>
+
+
+
+    </>
+
+
 
   )
 }
